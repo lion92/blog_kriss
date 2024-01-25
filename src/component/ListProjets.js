544dd03a6c2f471ff6b054ada007efc75283a133
lion92 <38532569+lion92@users.noreply.github.com>
2024-01-25 03:30:28 +0100

    intégration de ligne de code pour illustrer mon code

--- src/component/ListProjets.js
+++ src/component/ListProjets.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './css/listProjets.css'
-
+import Gist from "react-gist";
 import {NavLink} from "react-router-dom";
 import MenuItem from "@mui/material/MenuItem";
 
@@ -15,8 +15,12 @@ const ListProjets = () => {
             </NavLink></button>
 
             <h1>Comment créer un projet React app avec un minimum de chose:</h1>
-            <h2>J'ai crée un projet react app avec un router et material ui d'intégré pour pour utiliser la librairie de composant.</h2>
-
+            <h2>J'ai crée un projet react app avec un router et material ui d'intégré pour pour utiliser la librairie de
+                composant.</h2>
+            <p>Il suffit de faire un git clone de https://github.com/lion92/blog_kriss.git</p>
+            <div className="clone_git">
+                <Gist id="99b008bf95608a47a3c1db1ded4c257a"/>
+            </div>
 
         </div>
     );
